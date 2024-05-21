fa52c28f0b0315d6e3617f45983920d55dfb8567
Grupo Projeto <grupopiscc@gmail.com>
2024-05-21 03:34:00 -0300

    forgot CEP from addresses

--- database/migrations/2024_05_21_060157_create_employee_addresses_table.php
+++ database/migrations/2024_05_21_060157_create_employee_addresses_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('description');
             $table->integer('house_number');
             $table->string('extra_info');
+            $table->string('cep');
             $table->timestamps();
         });
     }
